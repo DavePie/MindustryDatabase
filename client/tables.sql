3161d2ef8b479916b95b5fc094f8506063d222c9
xasmedy <xasmedy08cim@gmail.com>
2025-02-08 23:10:11 +0100

    Migrated the tables.sql from mariadb to postgresql.

--- client/tables.sql
+++ client/tables.sql
@@ -4,30 +4,30 @@ CREATE TABLE IF NOT EXISTS account(
 
     id            SERIAL       PRIMARY KEY,
     username      VARCHAR(15)  NOT NULL UNIQUE, -- Anything bigger than 15 is quite long.
-    display_name  TINYTEXT     NOT NULL,
-    creation_date DATETIME(3)  NOT NULL DEFAULT UTC_TIMESTAMP(3),
-    -- argon2id variable length hash, I use TEXT because the hash can get very long by changing the settings, and I want to allow that for future proofing.
-    password      TEXT         NOT NULL,
+    display_name  VARCHAR(255) NOT NULL,
+    creation_date TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    -- Variable length hash, not fixed length since the hash can get very long by changing the settings, and I want to allow that for future proofing.
+    password      BYTEA        NOT NULL, -- Argon2id
 
     -- I use the discord username validation since they fit our use cases.
     CONSTRAINT chk_username_valid CHECK (
-            CHAR_LENGTH(username) > 2 AND
-            username REGEXP '^[a-z0-9_.]+$' AND
-            username NOT REGEXP '\\.\\.'
+            LENGTH(username) > 2 AND
+            username  ~ '^[a-z0-9_.]+$' AND
+            username !~ '\\.\\.'
     )
 );
 
 -- TODO Server authorization based on user role.
 CREATE TABLE IF NOT EXISTS server(
 
-    id               SERIAL      PRIMARY KEY,
-    ip_address       INET4       NOT NULL,
-    port             SMALLINT    NOT NULL,
-    name             TINYTEXT    NOT NULL,
+    id               SERIAL       PRIMARY KEY,
+    ip_address       INET         NOT NULL,
+    port             SMALLINT     NOT NULL,
+    name             VARCHAR(255) NOT NULL,
     -- Last heartbeat available used in case the server goes offline.
-    heartbeat        DATETIME(3) NOT NULL DEFAULT UTC_TIMESTAMP(3),
+    heartbeat        TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
     -- The period of the heartbeat in milliseconds.
-    heartbeat_period INT         NOT NULL DEFAULT 5000,
+    heartbeat_period INT          NOT NULL DEFAULT 5000,
 
     CONSTRAINT u_server_ip_port UNIQUE(ip_address, port)
 );
@@ -36,8 +36,8 @@ CREATE TABLE IF NOT EXISTS login(
 
     id         SERIAL      PRIMARY KEY,
     account_id INT         NOT NULL,
-    ip_address INET4       NOT NULL,
-    login_date DATETIME(3) NOT NULL DEFAULT UTC_TIMESTAMP(3),
+    ip_address INET        NOT NULL,
+    login_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_login_user FOREIGN KEY(account_id) REFERENCES account(id)
 );
@@ -46,10 +46,11 @@ CREATE TABLE IF NOT EXISTS account_session(
 
     id              SERIAL      PRIMARY KEY,
     account_id      INT         NOT NULL UNIQUE, -- There can be only one session at a time.
-    session_cookie  BINARY(32)  NOT NULL UNIQUE,
-    expiration_date DATETIME(3) NOT NULL DEFAULT UTC_TIMESTAMP(3),
+    session_cookie  BYTEA       NOT NULL UNIQUE, -- Sha256
+    expiration_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT fk_account_session FOREIGN KEY(account_id) REFERENCES account(id)
+    CONSTRAINT fk_account_session FOREIGN KEY(account_id) REFERENCES account(id),
+    CONSTRAINT chk_session_cookie_length CHECK (LENGTH(session_cookie) = 32)
 );
 
 CREATE TABLE IF NOT EXISTS server_join(
@@ -57,8 +58,8 @@ CREATE TABLE IF NOT EXISTS server_join(
     id         SERIAL      PRIMARY KEY,
     account_id INT         NOT NULL,
     server_id  INT         NOT NULL,
-    join_date  DATETIME(3) NOT NULL DEFAULT UTC_TIMESTAMP(3),
-    leave_date DATETIME(3) NULL     DEFAULT NULL,
+    join_date  TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    leave_date TIMESTAMPTZ NULL     DEFAULT NULL,
 
     CONSTRAINT fk_server_join_user   FOREIGN KEY(account_id) REFERENCES account(id),
     CONSTRAINT fk_server_join_server FOREIGN KEY(server_id)  REFERENCES server(id)
@@ -66,12 +67,12 @@ CREATE TABLE IF NOT EXISTS server_join(
 
 CREATE TABLE IF NOT EXISTS report(
 
-    id            SERIAL      PRIMARY KEY,
-    account_id    INT         NOT NULL,
-    reported_id   INT         NOT NULL,
-    short_reason  TINYTEXT    NOT NULL,
-    long_reason   TEXT        NOT NULL DEFAULT '',
-    creation_date DATETIME(3) NOT NULL DEFAULT UTC_TIMESTAMP(3),
+    id            SERIAL       PRIMARY KEY,
+    account_id    INT          NOT NULL,
+    reported_id   INT          NOT NULL,
+    short_reason  VARCHAR(255) NOT NULL,
+    long_reason   TEXT         NOT NULL DEFAULT '',
+    creation_date TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_report_account  FOREIGN KEY(account_id)  REFERENCES account(id),
     CONSTRAINT fk_report_reported FOREIGN KEY(reported_id) REFERENCES account(id)
@@ -84,7 +85,7 @@ CREATE TABLE IF NOT EXISTS report_reply(
     staff_id      INT         NOT NULL,
     accepted      BOOLEAN     NOT NULL,
     message       TEXT        NOT NULL DEFAULT '',
-    creation_date DATETIME(3) NOT NULL DEFAULT UTC_TIMESTAMP(3),
+    creation_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_report_reply_staff  FOREIGN KEY(staff_id)  REFERENCES account(id),
     CONSTRAINT fk_report_reply_report FOREIGN KEY(report_id) REFERENCES report(id) ON DELETE CASCADE
@@ -100,9 +101,9 @@ CREATE TABLE IF NOT EXISTS ban(
     server_id       INT         NOT NULL,
     reason          TEXT        NOT NULL,
     handled         BOOLEAN     NOT NULL DEFAULT FALSE, -- False if it needs to be handled by the server, true if it has been handled.
-    creation_date   DATETIME(3) NOT NULL DEFAULT UTC_TIMESTAMP(3),
+    creation_date   TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     -- Null for bans that are permanent.
-    expiration_date DATETIME(3) NULL,
+    expiration_date TIMESTAMPTZ NULL DEFAULT NULL,
 
     CONSTRAINT fk_ban_user   FOREIGN KEY(account_id) REFERENCES account(id),
     CONSTRAINT fk_ban_staff  FOREIGN KEY(staff_id)   REFERENCES account(id),
@@ -114,7 +115,7 @@ CREATE TABLE IF NOT EXISTS unban(
     id            SERIAL      PRIMARY KEY,
     ban_id        INT         NOT NULL UNIQUE,
     staff_id      INT         NOT NULL,
-    creation_date DATETIME(3) NOT NULL DEFAULT UTC_TIMESTAMP(3),
+    creation_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_unban_ban   FOREIGN KEY(ban_id)   REFERENCES ban(id),
     CONSTRAINT fk_unban_staff FOREIGN KEY(staff_id) REFERENCES account(id)
@@ -128,7 +129,7 @@ CREATE TABLE IF NOT EXISTS kick(
     server_id     INT         NOT NULL,
     reason        TEXT        NOT NULL,
     handled       BOOLEAN     NOT NULL DEFAULT FALSE, -- False if it needs to be handled by the server, true if it has been handled.
-    creation_date DATETIME(3) NOT NULL DEFAULT UTC_TIMESTAMP(3),
+    creation_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_kick_user   FOREIGN KEY(account_id) REFERENCES account(id),
     CONSTRAINT fk_kick_staff  FOREIGN KEY(staff_id)   REFERENCES account(id),
@@ -143,7 +144,7 @@ CREATE TABLE IF NOT EXISTS warn(
     server_id     INT         NOT NULL,
     reason        TEXT        NOT NULL,
     handled       BOOLEAN     NOT NULL DEFAULT FALSE, -- False if it needs to be handled by the server, true if it has been handled.
-    creation_date DATETIME(3) NOT NULL DEFAULT UTC_TIMESTAMP(3),
+    creation_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_warn_user   FOREIGN KEY(account_id) REFERENCES account(id),
     CONSTRAINT fk_warn_staff  FOREIGN KEY(staff_id)   REFERENCES account(id),
@@ -157,8 +158,8 @@ CREATE TABLE IF NOT EXISTS mute(
     staff_id        INT         NOT NULL,
     server_id       INT         NOT NULL,
     reason          TEXT        NOT NULL,
-    creation_date   DATETIME(3) NOT NULL,
-    expiration_date DATETIME(3) NOT NULL,
+    creation_date   TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    expiration_date TIMESTAMPTZ NOT NULL,
 
     CONSTRAINT fk_mute_user   FOREIGN KEY(account_id) REFERENCES account(id),
     CONSTRAINT fk_mute_staff  FOREIGN KEY(staff_id)   REFERENCES account(id),
@@ -171,7 +172,7 @@ CREATE TABLE IF NOT EXISTS ban_appeal(
     account_id    INT         NOT NULL,
     ban_id        INT         NOT NULL,
     message       TEXT        NOT NULL,
-    creation_date DATETIME(3) NOT NULL DEFAULT UTC_TIMESTAMP(3),
+    creation_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_ban_appeal_account FOREIGN KEY(account_id) REFERENCES account(id),
     CONSTRAINT fk_ban_appeal_ban     FOREIGN KEY(ban_id)     REFERENCES ban(id)
@@ -184,7 +185,7 @@ CREATE TABLE IF NOT EXISTS ban_appeal_reply(
     staff_id      INT         NOT NULL,
     accepted      BOOLEAN     NOT NULL,
     message       TEXT        NOT NULL DEFAULT '',
-    creation_date DATETIME(3) NOT NULL DEFAULT UTC_TIMESTAMP(3),
+    creation_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_ban_appeal_reply_ban   FOREIGN KEY(ban_appeal_id) REFERENCES ban_appeal(id),
     CONSTRAINT fk_ban_appeal_reply_staff FOREIGN KEY(staff_id)      REFERENCES account(id)
@@ -194,28 +195,28 @@ CREATE TABLE IF NOT EXISTS ip_blacklist(
 
     id            SERIAL      PRIMARY KEY,
     staff_id      INT         NOT NULL,
-    ip_address    INET4       NOT NULL UNIQUE,
+    ip_address    INET        NOT NULL UNIQUE,
     reason        TEXT        NOT NULL DEFAULT '',
-    creation_date DATETIME(3) NOT NULL DEFAULT UTC_TIMESTAMP(3),
+    creation_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_ip_blacklist_staff FOREIGN KEY(staff_id) REFERENCES account(id)
 );
 
 CREATE TABLE IF NOT EXISTS role(
 
-    id       SERIAL      PRIMARY KEY,
-    name     TINYTEXT    NOT NULL,
+    id       SERIAL       PRIMARY KEY,
+    name     VARCHAR(255) NOT NULL,
     -- The order of which the roles are displayed, smallest first.
-    priority TINYINT     NOT NULL,
-    symbol   VARCHAR(16) NOT NULL,
-    color    CHAR(8)     NOT NULL
+    priority SMALLINT     NOT NULL,
+    symbol   VARCHAR(16)  NOT NULL,
+    color    CHAR(8)      NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS permission(
 
-    id       SERIAL   PRIMARY KEY,
+    id       SERIAL       PRIMARY KEY,
     -- Lowercase with - as space separators.
-    property TINYTEXT NOT NULL UNIQUE
+    property VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS account_role(
