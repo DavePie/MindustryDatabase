baff5632b091528bc60ab9ae9f3826c27f615610
BnDLett <BnDLett@users.noreply.github.com>
2025-04-02 11:49:11 -0400

    refactor(:plugin): Removed old Java code
    Removed old Java code that wasn't being used.

--- src/net/ddns/mindustry/database/javaPlugin/commands/clientCommands/moderation/UnprivilegedCommands.java
+++
@@ -1,9 +0,0 @@
-package net.ddns.mindustry.database.javaPlugin.commands.clientCommands.moderation;
-
-import arc.util.CommandHandler;
-
-public class UnprivilegedCommands {
-    public static void load(CommandHandler handler) {
-
-    }
-}
