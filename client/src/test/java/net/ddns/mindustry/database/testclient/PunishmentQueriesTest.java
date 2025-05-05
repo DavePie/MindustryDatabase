19a912c4c8b67c93251b8706fc657a0ad69d1db0
xasmedy <xasmedy08cim@gmail.com>
2025-05-05 19:23:09 +0200

    Added `kick` and `warn` tests.

--- client/src/test/java/net/ddns/mindustry/database/testclient/PunishmentQueriesTest.java
+++ client/src/test/java/net/ddns/mindustry/database/testclient/PunishmentQueriesTest.java
@@ -5,15 +5,22 @@ import net.ddns.mindustry.database.client.Database;
 import net.ddns.mindustry.database.client.PunishmentListeners;
 import net.ddns.mindustry.database.client.PunishmentQueries;
 import net.ddns.mindustry.database.schema.tables.pojos.Ban;
+import net.ddns.mindustry.database.schema.tables.pojos.Kick;
+import net.ddns.mindustry.database.schema.tables.pojos.Warn;
 import net.ddns.mindustry.database.testclient.data.MockAccount;
 import net.ddns.mindustry.database.testclient.data.MockServer;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import java.time.Duration;
+import java.time.OffsetDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.IntConsumer;
 
+@Execution(ExecutionMode.CONCURRENT)
 public final class PunishmentQueriesTest {
 
     private static final String USER_PUNISHED = "punished";
@@ -51,16 +58,58 @@ public final class PunishmentQueriesTest {
     void ban(String reason) {
 
         final CompletableFuture<Integer> future = new CompletableFuture<>();
-        db.punishmentListeners().register(PunishmentListeners.Type.BAN, future::complete);
+        final IntConsumer listener = future::complete;
+        db.punishmentListeners().register(PunishmentListeners.Type.BAN, listener);
 
-        final var status = db.punishment().ban(USER_PUNISHED, USER_STAFF, reason, MockServer.fromDb(db), null);
+        final var status = db.punishment().ban(USER_PUNISHED, USER_STAFF, reason, MockServer.fromDb(db), (OffsetDateTime) null);
         if (!(status instanceof PunishmentQueries.Status.Ok<Ban>(Ban ban))) {
-            Assertions.fail("Could not add the ban: " + status);
+            Assertions.fail("Could not issue the ban: " + status);
             throw new IllegalStateException();
         }
         Assertions.assertTimeoutPreemptively(Duration.of(5, ChronoUnit.SECONDS), () -> {
             final int id = future.get();
             Assertions.assertEquals(ban.id(), id);
         });
+        db.punishmentListeners().unregister(PunishmentListeners.Type.BAN, listener);
+    }
+
+    @ParameterizedTest
+    @MethodSource("net.ddns.mindustry.database.testclient.data.MockPunishment#reasons")
+    void kick(String reason) {
+
+        final CompletableFuture<Integer> future = new CompletableFuture<>();
+        final IntConsumer listener = future::complete;
+        db.punishmentListeners().register(PunishmentListeners.Type.KICK, listener);
+
+        final var status = db.punishment().kick(USER_PUNISHED, USER_STAFF, reason, MockServer.fromDb(db));
+        if (!(status instanceof PunishmentQueries.Status.Ok<Kick>(Kick kick))) {
+            Assertions.fail("Could not issue the kick: " + status);
+            throw new IllegalStateException();
+        }
+        Assertions.assertTimeoutPreemptively(Duration.of(5, ChronoUnit.SECONDS), () -> {
+            final int id = future.get();
+            Assertions.assertEquals(kick.id(), id);
+        });
+        db.punishmentListeners().unregister(PunishmentListeners.Type.KICK, listener);
+    }
+
+    @ParameterizedTest
+    @MethodSource("net.ddns.mindustry.database.testclient.data.MockPunishment#reasons")
+    void warn(String reason) {
+
+        final CompletableFuture<Integer> future = new CompletableFuture<>();
+        final IntConsumer listener = future::complete;
+        db.punishmentListeners().register(PunishmentListeners.Type.WARN, listener);
+
+        final var status = db.punishment().warn(USER_PUNISHED, USER_STAFF, reason, MockServer.fromDb(db));
+        if (!(status instanceof PunishmentQueries.Status.Ok<Warn>(Warn warn))) {
+            Assertions.fail("Could not issue the warn: " + status);
+            throw new IllegalStateException();
+        }
+        Assertions.assertTimeoutPreemptively(Duration.of(5, ChronoUnit.SECONDS), () -> {
+            final int id = future.get();
+            Assertions.assertEquals(warn.id(), id);
+        });
+        db.punishmentListeners().unregister(PunishmentListeners.Type.WARN, listener);
     }
 }
