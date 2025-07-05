15942061bc16f261030f08d04497ee70a3789aef
xasmedy <xasmedy08cim@gmail.com>
2025-07-05 12:32:52 +0200

    Added and changed more appeals tests covering almost all methods.

--- client/src/test/java/net/ddns/mindustry/database/testclient/test/AppealTest.java
+++ client/src/test/java/net/ddns/mindustry/database/testclient/test/AppealTest.java
@@ -5,10 +5,7 @@ import net.ddns.mindustry.database.client.Database;
 import net.ddns.mindustry.database.testclient.DbInitialization;
 import net.ddns.mindustry.database.testclient.data.MockAccount;
 import net.ddns.mindustry.database.testclient.util.AccountUtil;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -16,7 +13,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import static net.ddns.mindustry.database.client.AppealQueries.Status.Ok;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@Execution(ExecutionMode.CONCURRENT)
+@Execution(ExecutionMode.SAME_THREAD) // Some tests might influence the others.
 public final class AppealTest {
 
     private final Database db;
@@ -30,25 +27,64 @@ public final class AppealTest {
         db.close();
     }
 
-    @ParameterizedTest
-    @MethodSource("net.ddns.mindustry.database.testclient.data.MockMessages#appeals")
-    void newAppeal(String message) {
+    @Order(0)
+    @Test
+    void newAppeal() {
 
         final var user  = AccountUtil.signupAssertive(db, MockAccount.random());
-        // Account Appeals
+
+        // ======= PRE APPEAL =======
+        // Empty Open Appeals
+        Assertions.assertTrue(db.appeal().openAppeals(Integer.MAX_VALUE).isEmpty());
+        Assertions.assertEquals(0, db.appeal().openAppealsCount());
+        // Empty Account Appeals
         Assertions.assertTrue(db.appeal().accountAppeals(user).isEmpty());
         Assertions.assertEquals(0, db.appeal().accountAppealsCount(user));
-        // Open Account Appeals
+        // Empty Open Account Appeals
         Assertions.assertTrue(db.appeal().openAccountAppeals(user).isEmpty());
         Assertions.assertEquals(0, db.appeal().openAccountAppealsCount(user));
 
-        final var appeal = Assertions.assertInstanceOf(Ok.class, db.appeal().appeal(user, message)).appeal();
+        // I issue the appeal
+        final var appeal = Assertions.assertInstanceOf(Ok.class, db.appeal().appeal(user, "some_message")).appeal();
+
+        // ======= POST APPEAL =======
+        // Open Appeals
+        Assertions.assertEquals(appeal.id(), db.appeal().openAppeals(Integer.MAX_VALUE).getFirst().id());
+        Assertions.assertEquals(1, db.appeal().openAppealsCount());
         // Account Appeals
         Assertions.assertEquals(appeal.id(), db.appeal().accountAppeals(user).getFirst().id());
         Assertions.assertEquals(1, db.appeal().accountAppealsCount(user));
         // Open Account Appeals
         Assertions.assertEquals(appeal.id(), db.appeal().openAccountAppeals(user).getFirst().id());
         Assertions.assertEquals(1, db.appeal().openAccountAppealsCount(user));
+    }
+
+    @Test
+    @Order(1)
+    void newAppealReply() {
+
+        final var user  = AccountUtil.signupAssertive(db, MockAccount.random());
+        final var staff = AccountUtil.signupAssertive(db, MockAccount.random());
+        final var appeal = Assertions.assertInstanceOf(Ok.class, db.appeal().appeal(user, "Some random appeal message.")).appeal();
+
+        // Multiple replies are accepted, in case one staff changes idea after a while or for other reasons.
+        final var appealReply1 = db.appeal().replyToAppeal(appeal, staff, "message_1", false);
+        final var appealReply2 = db.appeal().replyToAppeal(appeal, staff, "message_2", true);
+        // I make sure the reply closes the appeal.
+        Assertions.assertTrue(db.appeal().openAccountAppeals(user).isEmpty());
+        Assertions.assertEquals(0, db.appeal().openAccountAppealsCount(user));
+
+        final var replies = db.appeal().appealReplies(appeal);
+        Assertions.assertTrue(replies.contains(appealReply1));
+        Assertions.assertTrue(replies.contains(appealReply2));
+        Assertions.assertEquals(2, db.appeal().appealRepliesCount(appeal));
+    }
+
+    @ParameterizedTest
+    @MethodSource("net.ddns.mindustry.database.testclient.data.MockMessages#appeals")
+    void newAppeal(String message) {
+        final var user  = AccountUtil.signupAssertive(db, MockAccount.random());
+        Assertions.assertInstanceOf(Ok.class, db.appeal().appeal(user, message));
     }
 
     @Test
@@ -79,5 +115,31 @@ public final class AppealTest {
         // I make sure the reply closes the appeal.
         Assertions.assertTrue(db.appeal().openAccountAppeals(user).isEmpty());
         Assertions.assertEquals(0, db.appeal().openAccountAppealsCount(user));
+    }
+
+    @Test
+    void deleteAppeal() {
+
+        final var user  = AccountUtil.signupAssertive(db, MockAccount.random());
+        final var appeal = Assertions.assertInstanceOf(Ok.class, db.appeal().appeal(user, "Some random appeal message.")).appeal();
+
+        Assertions.assertEquals(1, db.appeal().accountAppealsCount(user));
+        db.appeal().deleteAppeal(appeal);
+        Assertions.assertEquals(0, db.appeal().accountAppealsCount(user));
+    }
+
+    @Test
+    void deleteAppealReply() {
+
+        final var user  = AccountUtil.signupAssertive(db, MockAccount.random());
+        final var staff = AccountUtil.signupAssertive(db, MockAccount.random());
+
+        final var appeal = Assertions.assertInstanceOf(Ok.class, db.appeal().appeal(user, "Some random appeal message.")).appeal();
+        final var appealReply = db.appeal().replyToAppeal(appeal, staff, "Some random appeal message.", true);
+
+        Assertions.assertEquals(0, db.appeal().openAccountAppealsCount(user));
+        db.appeal().deleteAppealReply(appealReply);
+        // The appeal re-opens since there's no longer any reply.
+        Assertions.assertEquals(1, db.appeal().openAccountAppealsCount(user));
     }
 }
